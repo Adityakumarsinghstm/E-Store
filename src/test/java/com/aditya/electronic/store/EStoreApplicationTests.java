7c50654f11e78408b3cd95d3443c86b99534bbf0
Aditya <adityasingh62065@gmail.com>
2024-08-25 15:44:43 +0530

    first commit

---
+++ src/test/java/com/aditya/electronic/store/EStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.electronic.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

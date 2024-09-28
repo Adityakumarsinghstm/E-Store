6f6d46427b46f9c0f067a31dfae1da4be9d59ff1
Aditya <adityasingh62065@gmail.com>
2024-09-28 10:16:51 +0530

    implement categoryService

---
+++ src/main/java/com/aditya/electronic/store/services/CategoryService.java
@@ -0,0 +1,18 @@
+package com.aditya.electronic.store.services;
+
+import com.aditya.electronic.store.dtos.CategoryDto;
+import com.aditya.electronic.store.dtos.PageableResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CategoryService {
+    CategoryDto create(CategoryDto categoryDto);
+
+    CategoryDto update (CategoryDto categoryDto, String categoryId);
+
+    void delete(String categoryId);
+
+    PageableResponse<CategoryDto> getAll(int pageNumber, int pageSize, String sortBy, String sortDir);
+
+    CategoryDto get(String categoryId);
+}

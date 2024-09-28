f632ae8b4f93acd84f4e2c6cf892ea507a9e577f
Aditya <adityasingh62065@gmail.com>
2024-09-28 09:28:37 +0530

    add Category and CategoryDto file

---
+++ src/main/java/com/aditya/electronic/store/dtos/CategoryDto.java
@@ -0,0 +1,25 @@
+package com.aditya.electronic.store.dtos;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CategoryDto {
+
+    private String id;
+    @NotBlank
+    @Min(value = 4,message = "title should be minimum 4 character !!")
+    private String title;
+    @NotBlank(message = "category description required !!")
+    private String description;
+    private String coverImage;
+}
+

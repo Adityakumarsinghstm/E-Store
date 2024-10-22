5b20594aefa7621b96879cc6a9217313677981cf
Aditya <adityasingh62065@gmail.com>
2024-10-22 14:39:33 +0530

    create Order and OrderItem Entity

---
+++ src/main/java/com/aditya/electronic/store/entities/Order.java
@@ -0,0 +1,40 @@
+package com.aditya.electronic.store.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    private String orderId;
+    private String orderStatus;
+
+    private String paymentStatus;
+    private int orderAmount;
+    @Column(length = 1000)
+    private String billingAddress;
+    private String billingName;
+    private String billingPhone;
+
+    private Date orderedDate;
+    private Date delhiveredDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    private List<OrderItems> orderItems = new ArrayList<>();
+}
--- src/main/java/com/aditya/electronic/store/entities/User.java
+++ src/main/java/com/aditya/electronic/store/entities/User.java
@@ -1,10 +1,10 @@
 package com.aditya.electronic.store.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -27,6 +27,11 @@ public class User {
     private String about;
     @Column(name = "user_image_name")
     private String imageName;
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
+    private List<Order> orders = new ArrayList<>();
+
+
 
 
 
